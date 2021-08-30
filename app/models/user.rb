03e446cf201eb1d8dc225e8e44112eff4289bf2e
david luis <davidluis2020@gmail.com>
2021-08-30 18:09:42 +0100

    ola validates done DAVID

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :socks
+  has_many :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :location, :gender, :date_of_birth, :phone_number, presence: true
+  validates :gender, inclusion: { in: %w[male female] }
 end
