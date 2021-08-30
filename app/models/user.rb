739269d9a086ccd54d3d9028cee73bd760213e71
Antonio Guerra <antonioguerrained@gmail.com>
2021-08-30 23:08:30 +0100

    Added days to the date_of_birth and the avatar field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :socks
   has_many :bookings
+  has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
