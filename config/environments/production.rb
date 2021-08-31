8fe490910afb472a36f5bcd9e3fde6101f520860
Antonio Guerra <antonioguerrained@gmail.com>
2021-08-31 08:50:55 +0100

    Removed line with bad code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,12 @@
-Xrequire "active_support/core_ext/integer/time"
+require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+
+  config.action_mailer.default_url_options = { host: '' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
