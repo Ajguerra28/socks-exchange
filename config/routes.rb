ee2e9a3ec366ee89a27903015a731a44cb01eeeb
david luis <davidluis2020@gmail.com>
2021-09-02 15:27:57 +0100

    working on profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :socks, except: :destroy
+  resources :users, only: :show
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
